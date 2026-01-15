2e32697b3765c64a4045c3759988871a4240fedc
0xmmalik <manavmalik36@gmail.com>
2026-01-14 22:29:11 -0500

    began update for 2026

--- assets/scripts/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".navigation-menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
